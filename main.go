f4fe73a8c9e58287a9f74bba2cf3b69be7d2572c
bastayanfa <bang.fugishysukea@gmail.com>
2021-08-06 09:24:31 +0700

    first commit

--- main.go
+++ main.go
@@ -7,48 +7,15 @@ import (
 	"net/http"
 	"strings"
 	"time"
-	"todo/todo"
 
-	"github.com/dgrijalva/jwt-go"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
+
+	"todo/todo"
 )
-
-func LoggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.RequestURI)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func AuthMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		log.Println(r.RequestURI)
-		next.ServeHTTP(rw, r)
-
-		tokenString := r.Header.Get("Authorization")
-
-		tokenString = strings.ReplaceAll(tokenString, "Bearer ", "")
-
-		mySigningKey := []byte("password")
-
-		_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-			}
-
-			return mySigningKey, nil
-		})
-
-		if err != nil {
-			rw.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-	})
-}
 
 func main() {
 	r := mux.NewRouter()
-	r.Use(LoggingMiddleware)
 
 	r.HandleFunc("/auth", func(rw http.ResponseWriter, r *http.Request) {
 		mySigningKey := []byte("password")
@@ -59,7 +26,6 @@ func main() {
 
 		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 		ss, err := token.SignedString(mySigningKey)
-
 		if err != nil {
 			rw.WriteHeader(http.StatusInternalServerError)
 			return
@@ -71,11 +37,44 @@ func main() {
 	})
 
 	api := r.NewRoute().Subrouter()
-	api.Use(AuthMiddleware)
-	api.HandleFunc("/todos", todo.AddTask).Methods(http.MethodPut)
-	api.HandleFunc("/todos", todo.GetTask).Methods(http.MethodGet)
+	api.Use(authMiddleware)
+
+	todoApp := todo.NewApp(todo.NewJSONSerializer())
+
+	r.HandleFunc("/todos", todoApp.AddTask).Methods(http.MethodPut)
+
 	api.HandleFunc("/todos/{index}", todo.SetDone).Methods(http.MethodPut)
 
-	err := http.ListenAndServe(":9090", r)
-	fmt.Print(err)
+	api.HandleFunc("/todos", todo.GetTask).Methods(http.MethodGet)
+
+	http.ListenAndServe(":9090", r)
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := r.Header.Get("Authorization")
+		tokenString = strings.ReplaceAll(tokenString, "Bearer ", "")
+
+		mySigningKey := []byte("password")
+		_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+
+			return mySigningKey, nil
+		})
+
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
--- todo/todo.go
+++ todo/todo.go
@@ -2,6 +2,7 @@ package todo
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -16,26 +17,59 @@ type Task struct {
 	Done  bool
 }
 
-type NewTasksTodo struct {
+type NewTaskTodo struct {
 	Task string `json:"task"`
 }
 
-func AddTask(rw http.ResponseWriter, r *http.Request) {
+type Serializer interface {
+	Decode(io.Reader, interface{}) error
+	Encode(io.Writer, interface{}) error
+}
+
+type JSONSerializer struct{}
+
+func (j JSONSerializer) Decode(r io.Reader, v interface{}) error {
+	return json.NewDecoder(r).Decode(v)
+}
+func (j JSONSerializer) Encode(w io.Writer, v interface{}) error {
+	return json.NewEncoder(w).Encode(v)
+}
+
+func NewJSONSerializer() JSONSerializer {
+	return JSONSerializer{}
+}
+
+type App struct {
+	serialize Serializer
+}
+
+func NewApp(serialize Serializer) *App {
+	return &App{
+		serialize: serialize,
+	}
+}
+
+func (app *App) AddTask(rw http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	var task NewTasksTodo
-	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
-		rw.WriteHeader(http.StatusBadGateway)
+	var task NewTaskTodo
+	if err := app.serialize.Decode(r.Body, &task); err != nil {
+		// if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	New(task.Task)
 }
 
-func GetTask(rw http.ResponseWriter, r *http.Request) {
-	if err := json.NewEncoder(rw).Encode(List()); err != nil {
-		rw.WriteHeader(http.StatusInternalServerError)
+func AddTask(rw http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var task NewTaskTodo
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
+	New(task.Task)
 }
 
 func SetDone(rw http.ResponseWriter, r *http.Request) {
@@ -48,6 +82,14 @@ func SetDone(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	tasks[i].Done = true
+
+}
+
+func GetTask(rw http.ResponseWriter, r *http.Request) {
+	if err := json.NewEncoder(rw).Encode(List()); err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
 
 func List() map[int]*Task {
